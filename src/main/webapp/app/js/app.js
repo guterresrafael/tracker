6b4310954450f1f99e7d9116832573eadaba0b51
Anderson Noguez <Anderson Noguez@Noguez-Note>
2015-05-13 00:14:14 -0300

    #1 Ajuste angular para acessar rest

--- src/main/webapp/app/js/app.js
+++ src/main/webapp/app/js/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var app = angular.module('tracker', ['ngRoute', 'ngResource']);
+var app = angular.module('trackerApp', ['ngRoute', 'ngResource']);
 
 app.config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/', {templateUrl: 'partials/registration.html'});
         $routeProvider.when('/login', {templateUrl: 'partials/login.html'});
-        $routeProvider.when('/home', {templateUrl: 'partials/home.html'});
+        $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeController as data'});
         $routeProvider.otherwise({redirectTo: '/login'});
     }]);
