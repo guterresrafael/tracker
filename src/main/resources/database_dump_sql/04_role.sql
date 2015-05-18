e0602e97f2b4ef78e2b975fdb8b0d5c75cefa6e7
Rafael Guterres <guterresrafael@gmail.com>
2015-05-18 02:01:15 -0300

    Adicionado suporte a autorizacao conforme permissoes do usuario

--- src/main/resources/database_dump_sql/04_role.sql
+++ src/main/resources/database_dump_sql/04_role.sql
@@ -1,2 +1,31 @@
-INSERT INTO `role` (`id`, `name`, `description`) VALUES (1, 'login', 'Login');
-INSERT INTO `role` (`id`, `name`, `description`) VALUES (2, 'admin', 'Admin');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'login', 'Application Login');
+
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'admin', 'Admin Full');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'admin_create', 'Admin Create');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'admin_read', 'Admin Read');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'admin_update', 'Admin Update');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'admin_delete', 'Admin Delete');
+
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'users', 'Users Full');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'users_create', 'Users Create');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'users_read', 'Users Read');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'users_update', 'Users Update');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'users_delete', 'Users Delete');
+
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'devices', 'Devices Full');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'devices_create', 'Devices Create');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'devices_read', 'Devices Read');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'devices_update', 'Devices Update');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'devices_delete', 'Devices Delete');
+
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'positions', 'Positions Full');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'positions_create', 'Positions Create');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'positions_read', 'Positions Read');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'positions_update', 'Positions Update');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'positions_delete', 'Positions Delete');
+
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'commands', 'Commands Full');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'commands_create', 'Commands Create');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'commands_read', 'Commands Read');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'commands_update', 'Commands Update');
+INSERT INTO `role` (`id`, `name`, `description`) VALUES (NULL, 'commands_delete', 'Commands Delete');
